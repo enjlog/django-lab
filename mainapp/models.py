a90a09c93d03fc6ea6be9115151c14aace2e0939
enjlog <m.farsi2229@gmail.com>
2020-09-19 19:44:19 +0430

    third commit

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,8 @@
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+class Mainapp(models.Model):
+
+    name = models.TextField()
+    def __str__(self):
+        return self.name
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'home', views.home ),
+    url(r'', views.home),
 ]
